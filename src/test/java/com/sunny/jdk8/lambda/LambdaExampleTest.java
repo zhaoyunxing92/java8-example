cc22f059d3405be13a144c482a9a7a13765085d6
zhaoyunxing92 <2385585770@qq.com>
2018-08-06 16:11:40 +0800

    添加：注释

---
+++ src/test/java/com/sunny/jdk8/lambda/LambdaExampleTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+
+/**
+ * @author zhaoyunxing92
+ * @class: com.sunny.jdk8.lambda.LambdaExampleTest
+ * @date: 2018-08-06 13:22
+ * @des:
+ */
+public class LambdaExampleTest {
+
+}
