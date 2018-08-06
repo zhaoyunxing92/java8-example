117982b4a8c0a295fb007532d79150eb6ed8bfb7
zhaoyunxing92 <2385585770@qq.com>
2018-08-06 16:46:15 +0800

    优化：采用策略模式修改代码

--- src/test/java/com/sunny/jdk8/lambda/LambdaExampleTest.java
+++ src/test/java/com/sunny/jdk8/lambda/LambdaExampleTest.java
@@ -20,20 +20,66 @@ public class LambdaExampleTest {
     private List<User> users = Arrays.asList(
             new User("王五", 25, 190),
             new User("赵六", 18, 175),
-            new User("小明", 15, 168),
-            new User("张三", 20, 180));
+            new User("小明", 21, 168),
+            new User("张三", 15, 180));
 
     /**
-     * 过滤年龄大于20的人
+     * 过滤年龄大于等于20的人
      */
     @Test
-    public void filterUserAgeGt20() {
+    public void filterUser1() {
+        List<User> filterUsers = filerUsersAgeGt20(users);
+        filterUsers.forEach(System.out::println);
+    }
+
+    /**
+     * 通过策略模式优化代码
+     */
+    @Test
+    public void filterUser2() {
+        List<User> filterUsers = filerUsersByFilter(users, new AgeFilter());
+        filterUsers.forEach(System.out::println);
+    }
+
+    /**
+     * 过滤身高大于等于180的
+     */
+    @Test
+    public void filterUser3() {
+        List<User> filterUsers = filerUsersByFilter(users, new HeightFilter());
+        filterUsers.forEach(System.out::println);
+    }
+
+    /**
+     * 1.存在硬编码，代码不够灵活
+     *
+     * @param users 用户
+     * @return 符合条件的用户
+     */
+    private List<User> filerUsersAgeGt20(List<User> users) {
         List<User> filterUsers = new ArrayList<>();
         for (User user : users) {
             if (user.getAge() >= 20) {
                 filterUsers.add(user);
             }
         }
-        filterUsers.forEach(System.out::println);
+        return filterUsers;
+    }
+
+    /**
+     * 2.采用策略模式优化代码
+     *
+     * @param users  用户
+     * @param filter 过滤器
+     * @return
+     */
+    private List<User> filerUsersByFilter(List<User> users, Filter<User> filter) {
+        List<User> filterUsers = new ArrayList<>();
+        for (User user : users) {
+            if (filter.matches(user)) {
+                filterUsers.add(user);
+            }
+        }
+        return filterUsers;
     }
 }
