9f86c822ae3bd17ba89aa7e0d755c5c0c0cc9877
zhaoyunxing92 <2385585770@qq.com>
2018-08-06 16:14:41 +0800

    添加：用户类

--- src/main/java/com/sunny/jdk8/lambda/User.java
+++ src/main/java/com/sunny/jdk8/lambda/User.java
@@ -7,7 +7,58 @@ package com.sunny.jdk8.lambda;
  * @author zhaoyunxing92
  * @class: com.sunny.jdk8.lambda.User
  * @date: 2018-08-06 11:34
- * @des:
+ * @des: 用户
  */
 public class User {
+    /**
+     * 姓名
+     */
+    private String name;
+    /**
+     * 年龄
+     */
+    private int age;
+    /**
+     * 身高
+     */
+    private int height;
+
+    public User(String name, int age, int height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", height=" + height +
+                '}';
+    }
 }
