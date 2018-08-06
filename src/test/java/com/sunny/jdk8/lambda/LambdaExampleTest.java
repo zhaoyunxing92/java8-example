51b3b683b64491b027eb064bc71b38a45a0ce01c
zhaoyunxing92 <2385585770@qq.com>
2018-08-06 16:22:41 +0800

    添加：过滤年龄大于20的方法

--- src/test/java/com/sunny/jdk8/lambda/LambdaExampleTest.java
+++ src/test/java/com/sunny/jdk8/lambda/LambdaExampleTest.java
@@ -1,11 +1,39 @@
-import static org.junit.Assert.*;
+package com.sunny.jdk8.lambda;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * @author zhaoyunxing92
  * @class: com.sunny.jdk8.lambda.LambdaExampleTest
  * @date: 2018-08-06 13:22
- * @des:
+ * @des: lambda的演化过程
  */
 public class LambdaExampleTest {
+    /**
+     * 初始化用户
+     */
+    private List<User> users = Arrays.asList(
+            new User("王五", 25, 190),
+            new User("赵六", 18, 175),
+            new User("小明", 15, 168),
+            new User("张三", 20, 180));
 
+    /**
+     * 过滤年龄大于20的人
+     */
+    @Test
+    public void filterUserAgeGt20() {
+        List<User> filterUsers = new ArrayList<>();
+        for (User user : users) {
+            if (user.getAge() >= 20) {
+                filterUsers.add(user);
+            }
+        }
+        filterUsers.forEach(System.out::println);
+    }
 }
