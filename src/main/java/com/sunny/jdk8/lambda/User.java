d712b1fe523b9057e3cbd1413f5703594b453d0a
zhaoyunxing92 <2385585770@qq.com>
2018-08-28 10:16:20 +0800

    添加：hashCode和equals方法

--- src/main/java/com/sunny/jdk8/lambda/User.java
+++ src/main/java/com/sunny/jdk8/lambda/User.java
@@ -59,6 +59,26 @@ public class User {
     }
 
     @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        if (age != user.age) return false;
+        if (height != user.height) return false;
+        return name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + age;
+        result = 31 * result + height;
+        return result;
+    }
+
+    @Override
     public String toString() {
         return "User{" +
                 "name='" + name + '\'' +
